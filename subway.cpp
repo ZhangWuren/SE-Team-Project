85181ed58152e1a12c029d2fd324c14ace93c2fd
ZhangWuren <954489951@qq.com>
2019-01-13 16:44:14 +0800

    定义subway.h头文件，处理输入

---
+++ subway.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+#include "fstream"
+#include "subway.h"
+#include "string"
+using namespace std;
+
+int main()
+{
+    string filename = "beijing-subway.txt";
+    Map map(filename);
+}
--- subway.h
+++ subway.h
@@ -1,53 +1,90 @@
 #ifndef SUBWAY_H
 #define SUBWAY_H
 #include "string"
-#define TOTAL 400
+#define _TOTAL 393
+#define _MAXS 100
 using namespace std;
 
 class Station //地铁站
 {
-  public:
-    Station(string &staname);
-    Station();
-    string getName();
-    void setLaststa(Station laststa);
-    void setNextsta(Station nextsta);
+public:
+  Station(string &staname){
 
-  private:
-    string m_staname;
-    Station laststa();
-    Station nextsta();
-    int m_transLine[3];
+  };
+  Station(){
+
+  };
+  string getName();
+  void setLaststa(Station laststa);
+  void setNextsta(Station nextsta);
+
+private:
+  string m_staname;
+  Station laststa();
+  Station nextsta();
+  int m_transLine[3];
 };
 
 class Line //地铁线
 {
-  public:
-    Line();
+public:
+  Line(){};
+  void setLine(string stations[_MAXS]);
 
-  private:
-    Station stations[10];
-    bool m_ifcircle;
+private:
+  Station stations[_MAXS];
+  int m_order;
+  string m_name;
+  bool m_ifcircle;
 };
 
 class Map //线路图
 {
-  public:
-    Map(string filename);
-    void outputLine(int &num);
-    void search(string &start, string &end);
+public:
+  Map(string filename);
 
-  private:
-    Line lines[23];
+  void outputLine(int &num);
+  void search(string &start, string &end);
+
+private:
+  Line lines[23];
 };
 
 class Matrix //邻接矩阵
 {
-  public:
-    Matrix(Map map, Station strat);
+public:
+  Matrix(Map map, Station strat);
 
-  private:
-    int m_matrix[TOTAL][TOTAL];
+private:
+  int m_matrix[_TOTAL][_TOTAL];
 };
 
+Map::Map(string filename)
+{
+  fstream fin(filename);
+  string readline[_MAXS];
+  int staorder = 0;
+  int lineorder = 0;
+  string linetype;
+  string templinetype;
+  while (getline(fin, readline[staorder]))
+  {
+    if (readline[staorder].empty()) //下一条线
+    {
+      int index = readline[staorder - 1].find(' ');
+      string sub = readline[staorder - 1].substr(0, index);
+      cout << sub << ' ' << staorder << endl;
+      staorder = 0;
+
+      this->lines[linetype].setLine(*readline);
+    }
+    else
+    {
+      staorder++;
+    }
+  }
+  int index = readline[staorder - 1].find(' ');
+  string sub = readline[staorder - 1].substr(0, index);
+  cout << sub << ' ' << staorder << endl;
+};
 #endif
